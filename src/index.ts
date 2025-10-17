6bc6c15a38bd6f08ad9abfbf3ba248c904ceb5e1
Abhishek Sharma <abhi761685@gmail.com>
2025-10-17 16:04:46 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", function (socket){
+    console.log("user connected")
+    socket.send("hello")
+})
